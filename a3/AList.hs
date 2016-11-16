a818395e2f73d951a6e28616c4bddbd54eec25e0
Filip Tomin <tominf6453@gmail.com>
2016-11-16 11:38:11 -0500

    Finished part 0 of A3, AList.hs

--- a3/AList.hs
+++ a3/AList.hs
@@ -17,16 +17,29 @@ type AList a b = [(a, b)]
 -- | Returns the value in the association list corresponding to the given key.
 --   Assumes that the key is in the association list.
 lookupA :: Eq a => AList a b -> a -> b
-lookupA alist key = undefined
+lookupA alist key | fst (head alist) == key = snd (head alist)
+                  | otherwise               = lookupA (tail alist) key
 
 -- | Returns a new association list which is the old one, except with 
 --   the new key-value pair inserted. However, it returns the *same* list
 --   if the key already exists in the list.
 insertA :: Eq a => AList a b -> (a, b) -> AList a b
-insertA alist (key, val) = undefined
+insertA alist (key, val) | containsA alist key == False = alist ++ [(key, val)]
+						 | otherwise 					= alist
+
+-- | Returns a boolean value depending on whether or not the AList contain
+--   the given key.
+containsA :: Eq a => AList a b -> a -> Bool
+containsA [] _      = False
+containsA alist key | fst (head alist) == key = True
+					| otherwise 			  = False
 
 -- | Returns a new association list which is the old one, except with 
 --   the value corresponding to the given key changed to the given new value.
 --   However, it returns the *same* list if the key doesn't appear in the list.
 updateA :: Eq a => AList a b -> (a, b) -> AList a b
-updateA alist (key, val) = undefined
+updateA [] (key, val) = []
+updateA (a:as) (key, val) = if (fst a) == key then 
+								(key, val) : as
+							else
+								a: (updateA as (key, val))
