72c222afacc5402620397883510ef0e4dc43f90f
Filip Tomin <tominf6453@gmail.com>
2016-12-03 16:26:09 -0500

    shit

--- a3/Mutation.hs
+++ a3/Mutation.hs
@@ -6,19 +6,19 @@ which you will use as the data structure for storing "mutable" data.
 
 -- **YOU MUST ADD ALL FUNCTIONS AND TYPES TO THIS LIST AS YOU CREATE THEM!!**
 module Mutation (
-	Memory, Pointer(..), Value(..),
-	Mutable, get, set, def,
-	StateOp(..),
-	(>>>), (>~>), returnVal,
-	alloc, free)
-	where
+    Memory, Pointer(..), Value(..),
+    Mutable, get, set, def,
+    StateOp(..),
+    (>>>), (>~>), returnVal,
+    alloc, free)
+    where
 
 import AList (AList, lookupA, insertA, updateA, containsA)
 
 -- A type representing the possible values stored in memory.
 data Value = IntVal Integer |
-			 BoolVal Bool
-			 deriving Show
+             BoolVal Bool
+             deriving Show
 
 -- A type representing a container for stored "mutable" values.
 type Memory = AList Integer Value
@@ -28,17 +28,17 @@ data Pointer a = P Integer deriving Show
 
 -- Type class representing a type which can be stored in "Memory".
 class Mutable a where
-	-- Look up a value in memory referred to by a pointer.
-	get :: Memory -> Pointer a -> a
+    -- Look up a value in memory referred to by a pointer.
+    get :: Memory -> Pointer a -> a
 
-	-- Change a value in memory referred to by a pointer.
-	-- Return the new memory after the update.
-	set :: Memory -> Pointer a -> a -> Memory
+    -- Change a value in memory referred to by a pointer.
+    -- Return the new memory after the update.
+    set :: Memory -> Pointer a -> a -> Memory
 
-	-- Create a new memory location storing a value, returning a new pointer
-	-- and the new memory with the new value.
-	-- Raise an error if the input Integer is already storing a value.
-	def :: Memory -> Integer -> a -> (Pointer a, Memory)
+    -- Create a new memory location storing a value, returning a new pointer
+    -- and the new memory with the new value.
+    -- Raise an error if the input Integer is already storing a value.
+    def :: Memory -> Integer -> a -> (Pointer a, Memory)
 
 extractInt :: Value -> Integer
 extractInt (IntVal x) = x
@@ -48,37 +48,37 @@ extractBool (BoolVal b) = b
 
 instance Mutable Integer where
 
-	get mem (P p) = if (containsA mem p) then
-						extractInt (lookupA mem p)
-					else
-						error "Doesn't exist"
+    get mem (P p) = if (containsA mem p) then
+                        extractInt (lookupA mem p)
+                    else
+                        error "Doesn't exist"
 
-	set mem (P p) val = if (containsA mem p) then
-							updateA mem (p, (IntVal val))
-						else
-							error "Doesn't exist"
+    set mem (P p) val = if (containsA mem p) then
+                            updateA mem (p, (IntVal val))
+                        else
+                            error "Doesn't exist"
 
-	def mem int val = if (containsA mem int) then
-						  error "Already exists"
-					  else
-						  (P int, insertA mem (int, (IntVal val)))
+    def mem int val = if (containsA mem int) then
+                          error "Already exists"
+                      else
+                          (P int, insertA mem (int, (IntVal val)))
 
 instance Mutable Bool where
 
-	get mem (P p) =	if (containsA mem p) then
-						extractBool (lookupA mem p)
-					else
-						error "Doesn't exist"
+    get mem (P p) = if (containsA mem p) then
+                        extractBool (lookupA mem p)
+                    else
+                        error "Doesn't exist"
 
-	set mem (P p) val = if (containsA mem p) then
-							updateA mem (p, (BoolVal val))
-						else
-							error "Doesn't exist"
+    set mem (P p) val = if (containsA mem p) then
+                            updateA mem (p, (BoolVal val))
+                        else
+                            error "Doesn't exist"
 
-	def mem int val = if (containsA mem int) then
-						  error "Already exists"
-					  else
-						  (P int, insertA mem (int, (BoolVal val)))
+    def mem int val = if (containsA mem int) then
+                          error "Already exists"
+                      else
+                          (P int, insertA mem (int, (BoolVal val)))
 
 
 -- StateOp declarations and such
@@ -95,15 +95,15 @@ returnVal val = (StateOp val)
 -- "then"
 (>>>) :: StateOp a -> StateOp b -> StateOp b
 op1 >>> op2 = \s ->
-	let (_, s1) = op1 s
-	in op2 s1
+    let (_, s1) = op1 s
+    in op2 s1
 
 -- "bind"
 (>~>) :: StateOp a -> (a -> StateOp b) -> StateOp b
 (f >~> g) s =
-	let (x, s1) = f s
-		newStateOp = g x
-	in newStateOp s1
+    let (x, s1) = f s
+        newStateOp = g x
+    in newStateOp s1
 
 -- | Memory allocation functions using StateOp
 -- Allocating memory
