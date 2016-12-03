c62c7efadc088915fe7daeded6346ef7d405bb40
br3nd4nn34l <br3nd4nn34l@gmail.com>
2016-12-03 16:09:02 -0500

    Made first version of pointerTest

--- a3/MutationUser.hs
+++ a3/MutationUser.hs
@@ -4,8 +4,7 @@ This file contains code which uses the mutation library found in Mutation.hs
 -}
 
 import Mutation (
-    get, set, def, Mutable, Pointer, Memory
-    )
+    get, set, def, Mutable, Pointer, StateOp(..), Memory)
 
 -- | Takes a number <n> and memory, and stores two new values in memory:
 --   - the integer (n + 3) at location 100
@@ -13,7 +12,14 @@ import Mutation (
 --   Return the pointer to each stored value, and the new memory.
 --   You may assume these locations are not already used by the memory.
 pointerTest :: Integer -> Memory -> ((Pointer Integer, Pointer Bool), Memory)
-pointerTest = undefined
+pointerTest num mem = ((firstPtr, secondPtr), secondMem)
+						where
+							firstPair = (def mem 100 (num + 3))
+							firstPtr = fst firstPair
+							firstMem = snd firstPair
+							secondPair = (def firstMem 500 (num > 0))
+							secondPtr = fst secondPair
+							secondMem = snd secondPair
 
 swap :: Mutable a => Pointer a -> Pointer a -> StateOp ()
 swap = undefined
