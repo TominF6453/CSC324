1e79c425a2bf618a6e9dec16d8381dbe1fe81016
br3nd4nn34l <br3nd4nn34l@gmail.com>
2016-12-03 18:45:34 -0500

    Used >>> in MutationUser instead of giant where statement. Experiencing compilation shenangigans

--- a3/MutationUser.hs
+++ a3/MutationUser.hs
@@ -4,7 +4,7 @@ This file contains code which uses the mutation library found in Mutation.hs
 -}
 
 import Mutation (
-    get, set, def, Mutable, Pointer, StateOp(..), Memory)
+    get, set, def, Mutable, Pointer, StateOp(..), Memory, (>>>), (>~>))
 
 -- | Takes a number <n> and memory, and stores two new values in memory:
 --   - the integer (n + 3) at location 100
@@ -12,13 +12,19 @@ import Mutation (
 --   Return the pointer to each stored value, and the new memory.
 --   You may assume these locations are not already used by the memory.
 pointerTest :: Integer -> Memory -> ((Pointer Integer, Pointer Bool), Memory)
-pointerTest num mem = ((firstPtr, secondPtr), secondMem) where
-							firstPair = (def mem 100 (num + 3))
-							firstPtr = fst firstPair
-							firstMem = snd firstPair
-							secondPair = (def firstMem 500 (num > 0))
-							secondPtr = fst secondPair
-							secondMem = snd secondPair
+pointerTest num mem = (registerInt >>> registerBool) mem where
+    registerInt = (def 100 (num + 3))
+    registerBool = (def 500 (num > 0))
+
+                        {-((firstPtr, secondPtr), secondMem) where
+                            firstLambda = (def 100 (num + 3))
+                            firstPair = firstLambda mem
+                            firstPtr = fst firstPair
+                            firstMem = snd firstPair
+                            secondLambda = (def 500 (num > 0))
+                            secondPair = secondLambda firstMem
+                            secondPtr = fst secondPair
+                            secondMem = snd secondPair-}
 
 swap :: Mutable a => Pointer a -> Pointer a -> StateOp ()
 swap (P p1) (P p2) = undefined
